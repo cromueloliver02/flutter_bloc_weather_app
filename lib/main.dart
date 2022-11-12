31276247baf2d108d9db2cdea1fbb03cde9f998c
cromueloliver02 <cromuelbarut@gmail.com>
2022-11-12 10:03:13 +0800

    init app

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+import './weather_app.dart';
+
+void main() => runApp(const WeatherApp());
---
+++ lib/weather_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'pages/pages.dart';
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Weather App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
